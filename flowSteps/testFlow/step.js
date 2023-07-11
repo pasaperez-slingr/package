1398cb101fe9a68c694470672ec336632e7f25b1
pasaperez <aperez@slingr.io>
2023-07-11 17:58:17 -0300

    1

---
+++ flowSteps/testFlow/step.js
@@ -0,0 +1,3 @@
+step.testFlow = function () {
+
+};
---
+++ scripts/test.js
@@ -0,0 +1,18 @@
+exports.callbackTest = function () {
+    sys.ui.sendMessage({
+        scope: 'uiService:testUiService.testUiService',
+        name: 'callbackTest',
+        config: {},
+        callbacks: {
+            callbackTest: function (originalMessage, callbackData) {
+                sys.logs.info('callbackTest');
+            }
+        }
+    });
+}
+
+exports.test = function () {
+    sys.logs.warn("test function");
+    log("test function");
+    return "test function";
+}
