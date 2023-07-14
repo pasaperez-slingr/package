2f1b8428f3c387c19eddd085effddad83981dbe3
pasaperez <aperez@slingr.io>
2023-07-14 17:40:57 -0300

    Mensaje del commit

--- scripts/test.js
+++ scripts/test.js
@@ -19,17 +19,17 @@ exports.test = function () {
 // alias commit='function _commit(){ next_version=$(git describe --tags --abbrev=0 | awk -F. -v OFS=. '\''{if (NF==1) {print "v0.0.1"} else {if (length($NF+1)>length($NF)) $(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)); print ""$0}}'\'' ) && git add . && git commit -m "$1" && git push && git tag -a "$next_version" -m "Mensaje del tag" && git push --tags && git push --delete origin $(git tag -l | sort -V | head -n -2) && git tag -l | sort -V | head -n -2 | xargs git tag -d && git fetch --tags; };_commit'
 
 /*
-
+,
+		{
+			"type": "listener",
+			"namespace": "webhook",
+			"path": "/listeners/listeners.js"
+		}
 ,
 		{
 			"type": "flowStep",
 			"namespace": "steps",
 			"path": "/flowSteps/testFlow"
-		},
-		{
-			"type": "uiService",
-			"namespace": "uiServices",
-			"path": "/uiServices/testUiService"
 		}
 
 */
